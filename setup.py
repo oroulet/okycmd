f2d880e52bdb5e13bb12bbf8482d0dac40e98cd1
Olivier <olivier@utopia>
2011-09-23 10:56:55 +0200

    add files to create debian package

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from distutils.command.install_data import install_data
+
+
+import glob
+import os
+
+
+
+
+setup (name = "onkyocmd", 
+        version = "0.01",
+        description = "Network client to onkyo receiver",
+        author = "Olivier R-D",
+        url = 'http://launchpad.net/onkyocmd',
+        py_modules=["onkyo"],
+        #packages = ["onkyocmd"],
+        #package_dir = {'icehms': 'src/python/icehms'},
+        license = "GNU General Public License",
+        
+        scripts = ["oky"]
+
+
+        )
+
+            #("/var/lib/icehms/", ["db"]) ]
+        #("bin", ["bin/cleaner_hms", "lsholons", "lstopics", "register_hms_services.py", "run_ice_servers.py", "update_hms_services"])
+
+
+
